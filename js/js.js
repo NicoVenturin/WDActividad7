d95eee5d75c420952695082c1883ffb9b39a0f60
NicoVenturin <145157147+NicoVenturin@users.noreply.github.com>
2024-09-02 16:23:19 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,7 +1,11 @@
-var botonIngresar = document.getElementById('botonIngresar');
+var botonIngresar = document.querySelector('#botonIngresar');
 var usuarioDropdown = document.querySelector('#salir');
-var botonLogOut = document.getElementById('botonLogOut');
+var botonLogOut = document.querySelector('#botonLogOut');
 
+botonIngresar.addEventListener('click', ()=>{
+    botonIngresar.style.display = "none";
+    usuarioDropdown.style.display = "block";
+})
 const botonIngre = () =>{
     botonIngresar.style.display = "none";
     usuarioDropdown.style.display = "block";
--- src/navbar.js
+++ src/navbar.js
@@ -41,16 +41,5 @@ const navbar = `
         </div>
       </nav>
 `;
-var botonIngresar = document.getElementById('botonIngresar');
-var usuarioDropdown = document.querySelector('#salir');
-var botonLogOut = document.getElementById('botonLogOut');
 
-const botonIngre = () =>{
-    botonIngresar.style.display = "none";
-    usuarioDropdown.style.display = "block";
-}
-const logOut = () =>{
-    botonIngresar.style.display = "block";
-    usuarioDropdown.style.display = "none";
-}
 export{navbar};
